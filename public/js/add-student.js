186dccc4f609afc30cf5e18dca28f0b8fde2226e
Maliha Arif <malihaarif@live.co.uk>
2022-10-24 17:07:04 +0100

    pasting data from form to console

--- public/js/add-student.js
+++ public/js/add-student.js
@@ -3,3 +3,16 @@ import { linkLists } from './link-lists.js';
 let countriesSelect = document.getElementById('country');
 let provincesSelect = document.getElementById('province');
 linkLists(countriesSelect, provincesSelect);
+
+let submitButton = document.getElementById('submit');
+let form = document.getElementById('personData');
+
+form.addEventListener('submit', function (event) {
+  let data = new FormData(form);
+
+  // console.log(data.get());
+  for (let entry of data) {
+    console.log(entry[1]);
+  }
+  event.preventDefault();
+});
